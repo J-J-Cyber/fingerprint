9be78be755dc5e64411e7314af02b639789b9a31
J-J-Cyber <56450446+J-J-Cyber@users.noreply.github.com>
2020-11-17 03:14:08 +0100

    call p5-script from within index.html body-tag

--- js/sketch.js
+++ js/sketch.js
@@ -6,4 +6,10 @@ function setup() {
 function draw() {
   // put drawing code here
   background(220);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
